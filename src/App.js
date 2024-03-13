e1ccf5d602609b5dd6da709ffcd21b1e5903200d
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-03-12 22:47:42 -0500

    Lifted the state up to the root/App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 // import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 function App() {
+
+  const [tasks, setTasks] = useState([{ //Using the shared state at the root component level
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2:30 PM",
+    reminder: true
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "Feb 6th at 1:30 PM",
+    reminder: true
+  },
+  {
+    id: 3,
+    text: "Grocery Shopping",
+    day: "Feb 5th at 2:30 PM",
+    reminder: true
+  }])
+
   return (
       <div className="App">
         {/* <h1>Hello from React</h1> */}
         {/* <h2>Hello</h2> */}
         <Header title="Task Tracker"/>
+        <Tasks tasks = {tasks}></Tasks>
       </div>
 
     
---
+++ src/components/Tasks.js
@@ -0,0 +1,14 @@
+
+const Tasks = ({tasks}) =>{
+//Since the state might get used/shared between different components, hence, shifted it to the root component/App component. If we had kept it in this component, won't be able to use it outside without making it messy/more code.
+  return (
+    <>
+
+      {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}
+
+    </>
+  );
+
+}
+
+export default Tasks;

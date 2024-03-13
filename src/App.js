fbd6c913fb1db34e360c22b117b7fae015f8877b
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-03-12 21:05:26 -0500

    Created task tracker app project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello from React</h1>
+        <h2>Hello</h2>
+      </div>
+    </>
+    
+  );
+}
+
+export default App;
